c4a0ceb4232426b091da4687b44fed61f292ad4c
hatone <t.oshima@cyberagentamerica.com>
2013-12-25 16:17:13 -0800

    Complite Lab1

---
+++ Lab1/App2/pages/itemDetail/itemDetail.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
+        // この関数は、ユーザーがこのページに移動するたびに呼び出されます。
+        // ページ要素にアプリケーションのデータを設定します。
+        ready: function (element, options) {
+            var item = options && options.item ? Data.resolveItemReference(options.item) : Data.items.getAt(0);
+            element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
+            element.querySelector("article .item-title").textContent = item.title;
+            element.querySelector("article .item-subtitle").textContent = item.preptime;
+            element.querySelector("article .item-image").src = item.backgroundImage;
+            element.querySelector("article .item-image").alt = item.shortTitle;
+
+            var ingredients = element.querySelector("article .item-ingredients");
+            for (var i = 0; i < item.ingredients.length; i++) {
+                var ingredient = document.createElement("h2");
+                ingredient.textContent = item.ingredients[i];
+                ingredient.className = "ingredient";
+                ingredients.appendChild(ingredient);
+            }
+
+            // Display cooking directions
+            element.querySelector("article .item-directions").textContent = item.directions;
+
+            element.querySelector(".content").focus();
+        }
+    });
+})();
