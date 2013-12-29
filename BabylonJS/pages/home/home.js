26547aaa11bc8f8ce2c21691cde9a37accf08066
hatone <t.oshima@cyberagentamerica.com>
2013-12-29 13:24:00 -0800

    Add BablonJS tutorial

---
+++ BabylonJS/pages/home/home.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var canvas = document.getElementById("renderCanvas");
+            var engine = new BABYLON.Engine(canvas, true);
+            var scene = new BABYLON.Scene(engine);
+
+            var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+            var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
+            var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
+
+            scene.activeCamera.attachControl(canvas);
+
+            engine.runRenderLoop(function () {
+                scene.render();
+            });
+
+        }
+    });
+})();
